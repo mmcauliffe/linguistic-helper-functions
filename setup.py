6a6de6724d7c7fbbe32ce91f2a1cbc7a5032d986
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2013-11-05 20:34:53 -0800

    Update packaging.

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from distutils.core import setup
 
 setup(
     name='linguistic-helper-functions',
-    version='0.1.23',
+    version='0.1.24',
     author='Michael McAuliffe',
     author_email='michael.e.mcauliffe@gmail.com',
-    packages=['linghelper','linghelper.media'],
+    packages=['linghelper',
+                'linghelper.media',
+                'linghelper.phonetics',
+                'linghelper.phonetics.vowels',
+                'linghelper.representations',
+                'linghelper.psycholinguistics',
+                'linghelper.phonetics',],
     url='http://pypi.python.org/pypi/linguistic-helper-functions/',
     license='LICENSE.txt',
     description='',
