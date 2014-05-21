c95ce640082dfb857ccba3fd3b89b70fed40796c
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-05-20 17:00:59 -0700

    Start to probabilistic formant tracking

--- linghelper/phonetics/vowels/probabilistic_formants.py
+++ linghelper/phonetics/vowels/probabilistic_formants.py
@@ -1,4 +1,13 @@
-import numpy
+from numpy import exp
 
 from linghelper.phonetics.representations.amplitude_envelopes import to_gammatone_envelopes
 
+def sigmoid(x,alpha,theta):
+    y = 1/(1+exp(-1*alpha*(x-theta)))
+    return y
+
+def auditory_representation(path,gammatone=False):
+    num_bands = 128
+    freq_lims = (80,8000)
+    
+    
