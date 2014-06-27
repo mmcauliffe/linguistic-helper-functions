8aaeeb7388240be8e930893b4552c2f38353f173
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-06-27 15:58:49 -0700

    Test reorganization

--- tests/distance/dtw.py
+++ tests/distance/dtw.py
@@ -15,22 +15,21 @@ class DTWTest(unittest.TestCase):
                             [2,7,9],
                             [1,5,8],
                             [7,4,9]])
+        self.r_dtw_output = 31.14363
+        self.r_dtw_output_norm = 3.114363
+        
     
     def test_dtw_unnorm(self):
         distmat = generate_distance_matrix(self.source, self.target)
         linghelper = dtw_distance(self.source,self.target,norm=False)
         
-        r_dtw_output = 31.14363
-        print(linghelper)
-        self.assertTrue(abs(r_dtw_output - linghelper) < 0.01)
+        self.assertTrue(abs(self.r_dtw_output - linghelper) < 0.01)
     
     def test_dtw_norm(self):
         distmat = generate_distance_matrix(self.source, self.target)
         linghelper = dtw_distance(self.source,self.target,norm=True)
         
-        r_dtw_output = 3.114363
-        print(linghelper)
-        self.assertTrue(abs(r_dtw_output - linghelper) < 0.01)
+        self.assertTrue(abs(self.r_dtw_output_norm - linghelper) < 0.01)
         
 if __name__ == '__main__':
     unittest.main()
